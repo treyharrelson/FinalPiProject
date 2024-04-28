f0bda852460a1f13783b20881287ff5b50d48089
Dylan <dylan.pellegrin32@gmail.com>
2024-04-27 20:59:49 -0500

    Switching Pages, Grid implemented

--- Switching Pages.py
+++ Switching Pages.py
@@ -8,34 +8,60 @@ running = False
 class Workout1:
     def __init__(self, root=None):
         self.root = root
-        self.frame = tk.Frame(self.root)
+        self.frame = tk.Frame(self.root, bg = "#749CBB")
         self.frame.pack()
-        
-        tk.Label(self.frame, text = "Workout One", anchor = "n", fg = "white", bg = "#749CBB", \
-              height = 1, width = 25, font = ("texgyreadventor-regular", 27)).pack() #place(x = 125, y = 0)
-
-        tk.Button(self.frame, text = 'Play', anchor = "n", fg = "white", bg = "#5B8C5D", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 50)).pack() #place(x = 165, y = 225)
-        
-        tk.Label(self.frame, text = "00:00.00", anchor = "n", fg = "black", bg = "white", \
-              height = 1, width = 7, font = ("texgyreadventor-regular", 40)).pack() #place(x = 285, y = 135)
-        
-        tk.Button(self.frame, text = 'Stop', anchor = "n", fg = "white", bg = "#9C4B60", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 50)).pack() #place(x = 425, y = 225)
-        
-        tk.Button(self.frame, text = 'Reset', anchor = "n", fg = "white", bg = "#748CBB", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 20)).pack() #place(x = 50, y = 400)
-        
-        tk.Button(self.frame, text = 'Next Workout', anchor = "n", fg = "white", bg = "#9E6CA8", \
-              height = 1, width = 11, font = ("texgyreadventor-regular", 20), command=self.make_page_2).pack() #place(x = 555, y = 10)
-        
-        tk.Label(self.frame, text='Weight Lifted (lbs):', bd = 6, font = ("texgyreadventor-regular", 10)).pack() #place(x= 10, y = 10)
-        
-        # entry = tk.Entry(self.frame, bd = 5, bg = 'white', font = 'Helvetica 14',  fg = 'black', width = 7, text = 'Weight Lifted',
-        #             anchor = "center",).place(x = 140, y = 10)
-        
+        self.setupGUI()
         self.page_2 = Workout2(master=self.root, app=self)
         self.page_3 = Workout3(master=self.root, app=self)
+    
+    # Initializes all widgets on the page
+    # Sets up 3 different sections on the page using tk.Frame
+    def setupGUI(self):
+        
+    # Frame 1 setup
+        self.frame1 = tk.Frame(self.frame, bg = "#749CBB")
+        
+        t1 = tk.Label(self.frame1, text = "Workout One", fg = "white",
+                      bg = "#749CBB", font = ("texgyreadventor-regular",30))
+        t1.grid(row=0, column=0, pady=(10,0), sticky="")
+        
+    # Frame 2 setup
+        self.frame2 = tk.Frame(self.frame, bg = "#749CBB")
+        
+        t3 = tk.Label(self.frame2, text = 'Weight Lifted (lbs):', font = ("texgyreadventor-regular", 10))
+        t3.grid(row=0, column=0, padx=(10,0), sticky="")
+        
+        entry = tk.Entry(self.frame2, bd = 5, width= 4, bg = 'white', font = ("texgyreadventor-regular"), 
+                         fg = 'black', text = 'Weight Lifted',)
+        entry.grid(row=0, column=1, padx=(5,345), sticky="")
+        
+        b4 = tk.Button(self.frame2, text = 'Next Workout', fg = "white", bg = "#9E6CA8",
+                       font = ("texgyreadventor-regular", 20), command=self.make_page_2)
+        b4.grid(row=0, column=2, padx=(0,10), sticky="")
+        
+        timer = tk.Label(self.frame2, text = "00:00.00", fg = "black", bg = "white",
+                         font = ("texgyreadventor-regular", 40))
+        timer.grid(row=1, column=0, columnspan=3, pady=(30,20), sticky="")
+        
+    # Frame 3 setup
+        self.frame3 = tk.Frame(self.frame, bg = "#749CBB")
+        
+        b1 = tk.Button(self.frame3, text = 'Play', fg = "white", bg = "#5B8C5D",
+                       font = ("texgyreadventor-regular", 50))
+        b1.grid(row=0, column=0, padx=30, sticky="")
+        
+        b2 = tk.Button(self.frame3, text = 'Stop', fg = "white", bg = "#9C4B60",
+                       font = ("texgyreadventor-regular", 50))
+        b2.grid(row=0, column=1, padx=30, sticky="")
+        
+        b3 = tk.Button(self.frame3, text = 'Reset', fg = "white", bg = "#748CBB",
+                       font = ("texgyreadventor-regular", 20))
+        b3.grid(row=1, column=1, pady=(30,10), sticky="")
+
+        # pack each section then pack the frame as a whole
+        self.frame1.pack()
+        self.frame2.pack()
+        self.frame3.pack()
     
     def main_page(self):
         self.frame.pack()
@@ -66,29 +92,57 @@ class Workout2:
     def __init__(self, master=None, app=None):
         self.master = master
         self.app = app
-        self.frame = tk.Frame(self.master)
+        self.frame = tk.Frame(self.master, bg = "#749CBB")
+        self.setupGUI()
     
-        t1 = tk.Label(self.frame, text = "Workout Two", anchor = "n", fg = "white", bg = "#749CBB", \
-              height = 1, width = 25, font = ("texgyreadventor-regular", 27)).pack() #place(x = 125, y = 0)
+    # Initializes all widgets on the page
+    # Sets up 3 different sections on the page using tk.Frame
+    def setupGUI(self):
         
-        b1 = tk.Button(self.frame, text = 'Play', anchor = "n", fg = "white", bg = "#5B8C5D", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 50)).pack() #place(x = 165, y = 225)
+    # Frame 1 setup
+        self.frame1 = tk.Frame(self.frame, bg = "#749CBB")
         
-        timer = tk.Label(self.frame, text = "00:00.00", anchor = "n", fg = "black", bg = "white", \
-              height = 1, width = 7, font = ("texgyreadventor-regular", 40)).pack() #place(x = 285, y = 135)
+        t1 = tk.Label(self.frame1, text = "Workout Two", fg = "white",
+                      bg = "#749CBB", font = ("texgyreadventor-regular",30))
+        t1.grid(row=0, column=0, pady=(10,0), sticky="")
         
-        b2 = tk.Button(self.frame, text = 'Stop', anchor = "n", fg = "white", bg = "#9C4B60", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 50)).pack() #place(x = 425, y = 225)
+    # Frame 2 setup
+        self.frame2 = tk.Frame(self.frame, bg = "#749CBB")
         
-        b3 = tk.Button(self.frame, text = 'Reset', anchor = "n", fg = "white", bg = "#748CBB", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 20)).pack() #place(x = 50, y = 400)
+        t3 = tk.Label(self.frame2, text = 'Weight Lifted (lbs):', font = ("texgyreadventor-regular", 10))
+        t3.grid(row=0, column=0, padx=(10,0), sticky="")
         
-        b4 = tk.Button(self.frame, text = 'Next Workout', anchor = "n", fg = "white", bg = "#9E6CA8", \
-              height = 1, width = 11, font = ("texgyreadventor-regular", 20), command=self.make_page_3).pack() #place(x = 555, y = 10)
+        entry = tk.Entry(self.frame2, bd = 5, width= 4, bg = 'white', font = ("texgyreadventor-regular"), 
+                         fg = 'black', text = 'Weight Lifted',)
+        entry.grid(row=0, column=1, padx=(5,345), sticky="")
         
-        t2 = tk.Label(self.frame, text='Weight Lifted (lbs):', bd = 6, font = ("texgyreadventor-regular", 10)).pack() #place(x= 10, y = 10)
-        # entry = tk.Entry(self.frame, bd = 5, bg = 'white', font = 'Helvetica 14',  fg = 'black', width = 7, text = 'Weight Lifted',
-        #             anchor = "center",).place(x = 140, y = 10)
+        b4 = tk.Button(self.frame2, text = 'Next Workout', fg = "white", bg = "#9E6CA8",
+                       font = ("texgyreadventor-regular", 20), command=self.make_page_3)
+        b4.grid(row=0, column=2, padx=(0,10), sticky="")
+        
+        timer = tk.Label(self.frame2, text = "00:00.00", fg = "black", bg = "white",
+                         font = ("texgyreadventor-regular", 40))
+        timer.grid(row=1, column=0, columnspan=3, pady=(30,20), sticky="")
+        
+    # Frame 3 setup
+        self.frame3 = tk.Frame(self.frame, bg = "#749CBB")
+        
+        b1 = tk.Button(self.frame3, text = 'Play', fg = "white", bg = "#5B8C5D",
+                       font = ("texgyreadventor-regular", 50))
+        b1.grid(row=0, column=0, padx=30, sticky="")
+        
+        b2 = tk.Button(self.frame3, text = 'Stop', fg = "white", bg = "#9C4B60",
+                       font = ("texgyreadventor-regular", 50))
+        b2.grid(row=0, column=1, padx=30, sticky="")
+        
+        b3 = tk.Button(self.frame3, text = 'Reset', fg = "white", bg = "#748CBB",
+                       font = ("texgyreadventor-regular", 20))
+        b3.grid(row=1, column=1, pady=(30,10), sticky="")
+
+        # pack each section then pack the frame as a whole
+        self.frame1.pack()
+        self.frame2.pack()
+        self.frame3.pack()
     
     def start_page(self):
         self.frame.pack()
@@ -101,29 +155,57 @@ class Workout3:
     def __init__(self, master=None, app=None):
         self.master = master
         self.app = app
-        self.frame = tk.Frame(self.master)
+        self.frame = tk.Frame(self.master, bg = "#749CBB")
+        self.setupGUI()
         
-        t1 = tk.Label(self.frame, text = "Workout Three", anchor = "n", fg = "white", bg = "#749CBB", \
-              height = 1, width = 25, font = ("texgyreadventor-regular", 27)).pack() #place(x = 125, y = 0)
+    # Initializes all widgets on the page
+    # Sets up 3 different sections on the page using tk.Frame
+    def setupGUI(self):
         
-        b1 = tk.Button(self.frame, text = 'Play', anchor = "n", fg = "white", bg = "#5B8C5D", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 50)).pack() #place(x = 165, y = 225)
+    # Frame 1 setup
+        self.frame1 = tk.Frame(self.frame, bg = "#749CBB")
         
-        timer = tk.Label(self.frame, text = "00:00.00", anchor = "n", fg = "black", bg = "white", \
-              height = 1, width = 7, font = ("texgyreadventor-regular", 40)).pack() #place(x = 285, y = 135)
+        t1 = tk.Label(self.frame1, text = "Workout Three", fg = "white",
+                      bg = "#749CBB", font = ("texgyreadventor-regular",30))
+        t1.grid(row=0, column=0, pady=(10,0), sticky="")
         
-        b2 = tk.Button(self.frame, text = 'Stop', anchor = "n", fg = "white", bg = "#9C4B60", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 50)).pack() #place(x = 425, y = 225)
+    # Frame 2 setup
+        self.frame2 = tk.Frame(self.frame, bg = "#749CBB")
         
-        b3 = tk.Button(self.frame, text = 'Reset', anchor = "n", fg = "white", bg = "#748CBB", \
-              height = 1, width = 5, font = ("texgyreadventor-regular", 20)).pack() #place(x = 50, y = 400)
+        t3 = tk.Label(self.frame2, text = 'Weight Lifted (lbs):', font = ("texgyreadventor-regular", 10))
+        t3.grid(row=0, column=0, padx=(10,0), sticky="")
         
-        b4 = tk.Button(self.frame, text = 'Next Workout', anchor = "n", fg = "white", bg = "#9E6CA8", \
-              height = 1, width = 11, font = ("texgyreadventor-regular", 20), command=self.go_back).pack() #place(x = 555, y = 10)
+        entry = tk.Entry(self.frame2, bd = 5, width= 4, bg = 'white', font = ("texgyreadventor-regular"), 
+                         fg = 'black', text = 'Weight Lifted',)
+        entry.grid(row=0, column=1, padx=(5,345), sticky="")
         
-        t2 = tk.Label(self.frame, text='Weight Lifted (lbs):', bd = 6, font = ("texgyreadventor-regular", 10)).pack() #place(x= 10, y = 10)
-        #entry = tk.Entry(self.frame, bd = 5, bg = 'white', font = 'Helvetica 14',  fg = 'black', width = 7, text = 'Weight Lifted',
-        #            anchor = "center",).place(x = 140, y = 10)
+        b4 = tk.Button(self.frame2, text = 'Next Workout', fg = "white", bg = "#9E6CA8",
+                       font = ("texgyreadventor-regular", 20), command=self.go_back)
+        b4.grid(row=0, column=2, padx=(0,10), sticky="")
+        
+        timer = tk.Label(self.frame2, text = "00:00.00", fg = "black", bg = "white",
+                         font = ("texgyreadventor-regular", 40))
+        timer.grid(row=1, column=0, columnspan=3, pady=(30,20), sticky="")
+        
+    # Frame 3 setup
+        self.frame3 = tk.Frame(self.frame, bg = "#749CBB")
+        
+        b1 = tk.Button(self.frame3, text = 'Play', fg = "white", bg = "#5B8C5D",
+                       font = ("texgyreadventor-regular", 50))
+        b1.grid(row=0, column=0, padx=30, sticky="")
+        
+        b2 = tk.Button(self.frame3, text = 'Stop', fg = "white", bg = "#9C4B60",
+                       font = ("texgyreadventor-regular", 50))
+        b2.grid(row=0, column=1, padx=30, sticky="")
+        
+        b3 = tk.Button(self.frame3, text = 'Reset', fg = "white", bg = "#748CBB",
+                       font = ("texgyreadventor-regular", 20))
+        b3.grid(row=1, column=1, pady=(30,10), sticky="")
+
+        # pack each section then pack the frame as a whole
+        self.frame1.pack()
+        self.frame2.pack()
+        self.frame3.pack()
     
     def start_page(self):
         self.frame.pack()
